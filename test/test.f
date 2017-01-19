ed85afcd299f6bdbdc21e760030d167068d4cfe1
Jon Lighthall <jon.lighthall@gmail.com>
2017-01-19 21:54:16 +0000

    added sleep; beeps three times

--- test/test.f
+++ test/test.f
@@ -2,4 +2,9 @@
       print *, "hello world"
       print *, "fortan"
       print *, char(7)
+      call sleep(1)
+      print *, char(7)
+      call sleep(1)
+      print *, char(7)
+      call sleep(1)
       end program hello
