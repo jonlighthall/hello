58bb3a252656a713bc4176ee01751249348eff7e
Jon Lighthall <jon.lighthall@gmail.com>
2017-02-01 11:55:22 -0500

    moving files to root directory... and removing test directory?

--- test/hello.f
+++
@@ -1,8 +0,0 @@
-      program hello
-      print *, "hello world"
-      print *, "fortan"
-      do 10 i=0,2,1 !print beeps at end of program
-         print *, " beep!", char(7)
-         call sleep(1)
- 10   continue
-      end program hello
