813ecfd16f399883c73ca903413c3666d7e034df
Jon Lighthall <jon.lighthall@gmail.com>
2018-11-19 10:17:28 -0600

    Restore sleep/beep functionality: update UNIX unistd.h to Windows windows.h for sleep function.

--- hello.c
+++ hello.c
@@ -1,8 +1,13 @@
 #include <stdio.h> //required for printf
+#include <windows.h> //required for Sleep
 
 int main()
 {
   printf("hello world\n");
   printf("C++\n");
+  for(int i=0;i<3;i++) {//print beeps at end of program
+    printf(" beep!\a\n");
+    Sleep(1000)
+  }
   return 0;
 }
