33ad1203a252f7a3af9548a4147d56e053560f35
Jon Lighthall <jon.lighthall@gmail.com>
2019-04-23 10:22:32 -0500

    unify comments and formatting

--- hello.sh
+++ hello.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
 echo 'hello world'
 echo 'bash'
-for i in {1..3}
-
+for i in {1..3} #print beeps at end of program
 do
-echo -e " beep!\a"
-sleep 1
+    echo -e " beep!\a"
+    sleep 1
 done
