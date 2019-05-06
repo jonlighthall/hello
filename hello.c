19893e289f78436b5ac8ef9389fb30c9cce4fbff
Jon Lighthall <jon.lighthall@gmail.com>
2019-05-06 12:44:11 -0500

    merge updates from master

--- hello.c
+++ hello.c
@@ -1,12 +1,11 @@
 #include <stdio.h> //required for printf
 //#include <unistd.h> //required for sleep
-
 int main()
 {
   printf("hello world\n");
   printf("C++\n");
-  for(int i=0;i<3;i++) {//print beeps at end of program
-    printf(" beep!\a\n");
+  for(int i=1;i<4;i++) {//print beeps at end of program
+    printf(" %d beeps\a\n",i);
     //sleep(1);
     system("sleep 1");
   }
--- hello.f
+++ hello.f
@@ -1,8 +1,8 @@
       program hello
-      print *, "hello world"
-      print *, "fortan"
-      do 10 i=0,2,1 !print beeps at end of program
-         print *, " beep!", char(7)
+      write(*,'(a)') "hello world"
+      write(*,'(a)') "fortan"
+      do 10 i=1,3,1             !print beeps at end of program
+         write(*,'(i2,2(a))')i," beeps", char(7)
          call sleep(1)
  10   continue
       end
