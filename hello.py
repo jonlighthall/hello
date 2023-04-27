0b0a4404254363d2042a1025867ddb832aeef2fc
Jon Lighthall <jon.lighthall@gmail.com>
2023-04-27 12:38:16 -0500

    update to pythyon 3

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import time #required for sleep
 print("hello world")
 print("python")
 for i in range(1,4): #print beeps at end of program
-    print(" %d beeps\a") %i
+    print(" %d beeps\a" % i)
     time.sleep(1)
