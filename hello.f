1b2ed69dd5efeafb554af67a8732bfc8f258f4b3
Jon Lighthall <jon.lighthall@gmail.com>
2023-04-30 12:13:09 -0500

    update hello.f to f2018

--- hello.f
+++ hello.f
@@ -3,12 +3,12 @@
       integer i
       write(*,'(a)') "hello world"
       write(*,'(a)') "fortan"
-      do 10 i=1,3,1             !print beeps at end of program
+      do i=1,3,1             !print beeps at end of program
          if(i.eq.1) then
             write(*,'(i2,2(a))')i," beep", char(7)
          else
             write(*,'(i2,2(a))')i," beeps", char(7)
          endif
          call sleep(1)
- 10   continue
+      enddo
       end 
